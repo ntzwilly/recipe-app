fe2dd66e97963dc8097dbe704d3f45904d098622
ntzwilly <ntzwilly@gmail.com>
2022-02-07 18:38:19 +0200

    Fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[ show destroy ]
+  before_action :set_food, only: %i[show destroy]
 
   def index
     @foods = Food.all
@@ -14,7 +14,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: "Food was successfully created." }
+        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -25,17 +25,17 @@ class FoodsController < ApplicationController
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: "Food was successfully destroyed." }
+      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
     end
   end
 
   private
 
-    def set_food
-      @food = Food.find(params[:id])
-    end
+  def set_food
+    @food = Food.find(params[:id])
+  end
 
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
