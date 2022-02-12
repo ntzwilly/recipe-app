a235ac1cd355e92cc52cfac2bdf3c37a45c9ee84
ntzwilly <ntzwilly@gmail.com>
2022-02-12 23:07:56 +0200

    Add tests using RSpec and Capybara testing libraries.

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,6 @@ class Food < ApplicationRecord
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true
+
+  validates :price, numericality: { greater_than: 0 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,5 @@ class Recipe < ApplicationRecord
   has_many :foods, through: :recipe_foods
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
+  validates :preparation_time, :cooking_time, numericality: { greater_than: 0 }
 end
